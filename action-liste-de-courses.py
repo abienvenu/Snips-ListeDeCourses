c30b0efe87cc4142be70bab6c929e56458379784
Arnaud Bienvenu <abi@abienvenu.net>
2019-08-06 18:19:55 +0200

    add intents

--- action-liste-de-courses.py
+++ action-liste-de-courses.py
@@ -6,7 +6,23 @@ from hermes_python.ffi.utils import MqttOptions
 
 
 def add_item(item):
-    return "Ok, il faut ajouter {}".format(item)
+    return "Ok, il faut ajouter {} sur la liste de courses".format(item)
+
+
+def del_item(item):
+    return "Ok, il ne faut plus acheter de {}".format(item)
+
+
+def get_list():
+    return "Voici ce qu'il y a sur la liste de courses"
+
+
+def del_list():
+    return "Il faut purger la liste de courses"
+
+
+def send_sms():
+    return "Il faut envoyer la liste par SMS"
 
 
 def intent_callback(hermes, intent_message):
@@ -14,6 +30,14 @@ def intent_callback(hermes, intent_message):
     result = None
     if intent_name == "addItem":
         result = add_item(intent_message.slots.Item.first().value)
+    if intent_name == "delItem":
+        result = del_item(intent_message.slots.Item.first().value)
+    if intent_name == "getList":
+        result = get_list()
+    if intent_name == "delList":
+        result = del_list()
+    if intent_name == "sendSMS":
+        result = send_sms()
 
     if result is not None:
         hermes.publish_end_session(intent_message.session_id, result)

0e2db4dd50a4b5cb6b76f9a22feb9eb443f80470
Arnaud Bienvenu <abi@abienvenu.net>
2019-08-06 21:20:17 +0200

    full fonctionnel

--- action-liste-de-courses.py
+++ action-liste-de-courses.py
@@ -1,28 +1,90 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
+import configparser
+import io
+from requests import post
 from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
 
 
+class SnipsConfigParser(configparser.SafeConfigParser):
+    def to_dict(self):
+        return {
+            section: {
+                option_name: option
+                for option_name, option in self.items(section)
+            }
+            for section in self.sections()
+        }
+
+
+def read_configuration_file():
+    try:
+        with io.open(
+            "config.ini",
+            encoding="utf-8"
+        ) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, configparser.Error):
+        return dict()
+
+
+def load_list():
+    try:
+        with open("liste.txt", "r") as infile:
+            return set(json.load(infile))
+    except IOError:
+        return set()
+
+
+def save_list(data):
+    with open("liste.txt", "w") as outfile:
+        json.dump(list(data), outfile)
+
+
 def add_item(item):
-    return "Ok, il faut ajouter {} sur la liste de courses".format(item)
+    liste = load_list()
+    if item in liste:
+        return "Il y a déjà {} sur la liste de courses".format(item)
+    liste.add(item)
+    save_list(liste)
+    return "J'ai ajouté {} sur la liste de courses".format(item)
 
 
 def del_item(item):
-    return "Ok, il ne faut plus acheter de {}".format(item)
+    liste = load_list()
+    if item not in liste:
+        return "Il n'y a pas de {} sur la liste de courses".format(item)
+    liste.remove(item)
+    save_list(liste)
+    return "J'ai supprimé {} de la liste de courses".format(item)
 
 
 def get_list():
-    return "Voici ce qu'il y a sur la liste de courses"
+    liste = load_list()
+    return "Voici ce qu'il y a sur la liste de courses: {}".format(
+        ", ".join(liste))
 
 
 def del_list():
-    return "Il faut purger la liste de courses"
+    save_list(set())
+    return "J'ai purgé la liste de courses"
 
 
 def send_sms():
-    return "Il faut envoyer la liste par SMS"
+    liste = load_list()
+    config = read_configuration_file()
+    smsData = {
+        "user": config.get('user'),
+        "pass": config.get('pass'),
+        "msg": "Liste de courses: {}".format(", ".join(liste))
+    }
+    post("https://smsapi.free-mobile.fr/sendmsg", json=smsData)
+    return "J'ai envoyé la liste de courses par SMS"
 
 
 def intent_callback(hermes, intent_message):
